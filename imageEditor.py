34ecb068c8ea3b256ce0512792639c4fcb10337b
Joe Dooher <jdooher0@gmailcom>
2024-03-05 21:52:01 -0600

    initial tests with opencv functions

---
+++ imageEditor.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+import os
+
+def kmeans_color_quantization(image, clusters=8, rounds=1):
+    h, w = image.shape[:2]
+    samples = np.zeros([h*w,3], dtype=np.float32)
+    count = 0
+
+    for x in range(h):
+        for y in range(w):
+            samples[count] = image[x][y]
+            count += 1
+
+    compactness, labels, centers = cv2.kmeans(samples,
+            clusters, 
+            None,
+            (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10000, 0.0001), 
+            rounds, 
+            cv2.KMEANS_RANDOM_CENTERS)
+
+    centers = np.uint8(centers)
+    res = centers[labels.flatten()]
+    return res.reshape((image.shape))
+def resize(img,scale_percent):
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    # resize image
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA) 
+    return resized
+
+if __name__=="__main__":
+    imageFile=os.path.join(os.getcwd(),"paintByNumbers","test_images","concrete.jpg")
+    image = cv2.imread(imageFile)
+    result = kmeans_color_quantization(image, clusters=5)
+    resultResize=resize(result,100)
+    cv2.imshow('result', resultResize)
+    cv2.waitKey()  
